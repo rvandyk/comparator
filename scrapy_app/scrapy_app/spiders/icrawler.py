6c3de11604ed89e66a4364fd36a79cec0c8d830d
myreage <brassenslebg@gmail.com>
2018-06-08 14:14:23 +0200

    req

--- scrapy_app/scrapy_app/spiders/icrawler.py
+++ scrapy_app/scrapy_app/spiders/icrawler.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import SitemapSpider, Rule
+from scrapy.spiders import CrawlSpider, Rule
 from ..items import Product
 import json
 import logging
 
 
 
-class IcrawlerSpider(SitemapSpider):
+class IcrawlerSpider(CrawlSpider):
     name = 'icrawler'
 
 
@@ -17,15 +17,20 @@ class IcrawlerSpider(SitemapSpider):
         # To make everything dynamic, we need to override them inside __init__ method
         self.url = kwargs.get('url')
         self.domain = kwargs.get('domain')
-        self.sitemap_urls = [self.url]
+        self.start_urls = [self.url]
         self.allowed_domains = [self.domain]
         self.attributesJson = kwargs.get('attributesJson')
 
+        IcrawlerSpider.rules = [
+            Rule(LinkExtractor(unique=True), callback='parse_item', follow=True),
+        ]
+
         super(IcrawlerSpider, self).__init__(*args, **kwargs)
 
-    def parse(self, response):
+    def parse_item(self, response):
         # You can tweak each crawled page here
         # Don't forget to return an object.
+
         att_dict = json.loads(self.attributesJson)
         elt = dict()
         for k, v in att_dict.items():
